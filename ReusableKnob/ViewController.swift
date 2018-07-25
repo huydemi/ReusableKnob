3144c9cc7a1293f7d3f2ea9657ab9b19588049c2
Dang Quoc Huy <huysamdua@yahoo.com>
2018-07-25 11:11:56 +0700

    complete project

--- ReusableKnob/ViewController.swift
+++ ReusableKnob/ViewController.swift
@@ -33,16 +33,36 @@ class ViewController: UIViewController {
   @IBOutlet var valueLabel: UILabel!
   @IBOutlet var valueSlider: UISlider!
   @IBOutlet var animateSwitch: UISwitch!
-
+  @IBOutlet var knob: Knob!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    knob.lineWidth = 4
+    knob.pointerLength = 12
+    knob.setValue(valueSlider.value)
+    knob.addTarget(self, action: #selector(ViewController.handleValueChanged(_:)), for: .valueChanged)
+    
+    updateLabel()
   }
   
   @IBAction func handleValueChanged(_ sender: Any) {
-
+    if sender is UISlider {
+      knob.setValue(valueSlider.value)
+    } else {
+      valueSlider.value = knob.value
+    }
+    updateLabel()
   }
   
   @IBAction func handleRandomButtonPressed(_ sender: Any) {
-
+    let randomValue = Float(arc4random_uniform(101)) / 100.0
+    knob.setValue(randomValue, animated: animateSwitch.isOn)
+    valueSlider.setValue(Float(randomValue), animated: animateSwitch.isOn)
+    updateLabel()
+  }
+  
+  private func updateLabel() {
+    valueLabel.text = String(format: "%.2f", knob.value)
   }
 }
